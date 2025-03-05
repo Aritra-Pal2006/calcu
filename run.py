65cf99c5caad54b94819346596bb2fb4b5491da0
Aritra-Pal2006 <aritrapalofficial2006@gmail.com>
2025-03-05 23:35:21 +0530

    routes connected and ready to use

--- run.py
+++ run.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app
+@app.route('/addition')
+def addition():
+    return render_template("addition.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 

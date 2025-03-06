78f3a84910569abf70accd8bb4c564db73030ae6
Aritra-Pal2006 <aritrapalofficial2006@gmail.com>
2025-03-06 12:16:27 +0530

    modifies sub and added sub.css

--- run.py
+++ run.py
@@ -15,6 +15,15 @@ def addition():
         return render_template("addition.html", result=result)
     return render_template("addition.html")
 
+@app.route('/subtraction', methods=['GET', 'POST'])
+def subtraction():
+    if request.method == "POST" :
+        num1=request.form['num1']
+        num2=request.form['num2']
+        result=int(num1)-int(num2)
+        return render_template("sub.html", result=result)
+    return render_template("sub.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
